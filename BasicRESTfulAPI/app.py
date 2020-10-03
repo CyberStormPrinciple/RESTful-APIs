3e692b4c4e6aae86237b09bf9f0a6c96f1260351
Nathan Gilbert <weneedmoreminerals@gmail.com>
2020-10-03 16:01:51 -0400

    Added put and delete functionality

--- BasicRESTfulAPI/app.py
+++ BasicRESTfulAPI/app.py
@@ -28,6 +28,22 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        request_data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': request_data['price']}
+            items.append(item)
+        else:
+            item.update(request_data)
+        return item
+
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
