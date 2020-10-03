afab2b438529b307a2e4be3cd70c0157ec6168bc
Nathan Gilbert <weneedmoreminerals@gmail.com>
2020-10-03 15:48:57 -0400

    added authentication using postman to test

--- BasicRESTfulAPI/app.py
+++ BasicRESTfulAPI/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'komorek' # This is insecure
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -17,7 +24,7 @@ class Item(Resource):
             'message': "An item with name '{}' already exists.".format(name)
             }, 400
         request_data = request.get_json()
-        item = {'name':name, 'price':date['price']}
+        item = {'name':name, 'price':request_data['price']}
         items.append(item)
         return item, 201
 
@@ -26,7 +33,7 @@ class ItemList(Resource):
         return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
-api.add_resource(Item, '/items')
+api.add_resource(ItemList, '/items')
 
 
 app.run(port=5000, debug=True)
